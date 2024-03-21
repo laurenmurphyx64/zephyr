5835b2fd276f7a8e0008032b3069ae86e8f8a8f1
Lauren Murphy <lauren.murphy@intel.com>
2024-03-21 13:18:29 -0700

    working app
    works except for userspace
    
    Signed-off-by: Lauren Murphy <lauren.murphy@intel.com>

--- tests/subsys/llext/simple/src/threads_kernel_objects_ext.c
+++ tests/subsys/llext/simple/src/threads_kernel_objects_ext.c
@@ -17,7 +17,7 @@
 static struct k_thread my_thread;
 static K_THREAD_STACK_DEFINE(my_thread_stack, STACK_SIZE);
 
-// struct k_sem my_sem;
+struct k_sem my_sem;
 
 /* Deleting exactly 1 character from any of the test_thread printk
  * statements OR the 2 printk statements before the last
@@ -26,31 +26,31 @@ static K_THREAD_STACK_DEFINE(my_thread_stack, STACK_SIZE);
 
 void test_thread(void *arg0, void *arg1, void *arg2)
 {
-	printk("Thread printk 1\n"); 
-	printk("Thread printk 2\n");
-	printk("Thread printk 3\n");
+	// printk("Thread printk 1\n"); 
+	// printk("Thread printk 2\n");
+	// printk("Thread printk 3\n");
 	// printk("Take semaphore from test thread\n");
-	// k_sem_take(&my_sem, K_FOREVER);
+	k_sem_take(&my_sem, K_FOREVER);
 	// printk("Semaphore taken\n");
 }
 
 void test_entry(void)
 {
-	printk("Main printk 1\n");
-	printk("Main printk\n"); // 2
-	printk("M\n"); // ain printk 3
+	// printk("Main printk 1\n");
+	// printk("Main printk 2\n"); // 2
+	// printk("Main printk 3\n"); // ain printk 3
 	// printk("Initialize semaphore with initial count 0 and max 1\n");
-	// k_sem_init(&my_sem, 1, 1);
+	k_sem_init(&my_sem, 1, 1);
 	// printk("Give semaphore from main thread\n");
-	// k_sem_give(&my_sem);
+	k_sem_give(&my_sem);
 	
-	printk("Creating thread\n");
+	// printk("Creating thread\n");
 	k_tid_t tid = k_thread_create(&my_thread, my_thread_stack, STACK_SIZE,
 				&test_thread, NULL, NULL, NULL,
 				0, 0, K_FOREVER);
-	printk("Starting thread\n");
+	// printk("Starting thread\n");
 	k_thread_start(tid);
 	k_thread_join(&my_thread, K_FOREVER);
-	printk("Thread joined\n");
+	// printk("Thread joined\n");
 }
 LL_EXTENSION_SYMBOL(test_entry);
