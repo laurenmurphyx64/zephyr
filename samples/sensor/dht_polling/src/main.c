a9a8fe4f49b452c3acd7d250c83b3390723493f1
Lauren Murphy <lauren.murphy@intel.com>
2024-04-15 15:50:08 -0700

    samples: sensor: update dht_polling to new API
    Updates dht_polling sample to new sensor API.
    
    Signed-off-by: Lauren Murphy <lauren.murphy@intel.com>

--- samples/sensor/dht_polling/src/main.c
+++ samples/sensor/dht_polling/src/main.c
@@ -8,9 +8,12 @@
 #include <stdlib.h>
 
 #include <zephyr/device.h>
-#include <zephyr/drivers/sensor.h>
 #include <zephyr/sys/util_macro.h>
 #include <zephyr/kernel.h>
+#include <zephyr/drivers/sensor.h>
+#include <zephyr/drivers/sensor_data_types.h>
+#include <zephyr/rtio/rtio.h>
+#include <zephyr/dsp/print_format.h>
 
 #define DHT_ALIAS(i) DT_ALIAS(_CONCAT(dht, i))
 #define DHT_DEVICE(i, _)                                                                 \
@@ -18,6 +21,10 @@
 
 /* Support up to 10 temperature/humidity sensors */
 static const struct device *const sensors[] = {LISTIFY(10, DHT_DEVICE, ())};
+
+SENSOR_DT_READ_IODEV(dht_iodev, DT_ALIAS(dht0),
+		SENSOR_CHAN_AMBIENT_TEMP);
+RTIO_DEFINE(dht_ctx, 1, 1);
 
 int main(void)
 {
@@ -34,27 +41,60 @@ int main(void)
 		for (size_t i = 0; i < ARRAY_SIZE(sensors); i++) {
 			struct device *dev = (struct device *)sensors[i];
 
-			rc = sensor_sample_fetch(dev);
-			if (rc < 0) {
-				printk("%s: sensor_sample_fetch() failed: %d\n", dev->name, rc);
+			uint8_t buf[128];
+			rc = sensor_read(&dht_iodev, &dht_ctx, buf, 128);
+			if (rc != 0) {
+				printk("%s: sensor_read() failed: %d\n", dev->name, rc);
 				return rc;
 			}
 
-			struct sensor_value temp;
-			struct sensor_value hum;
+			const struct sensor_decoder_api *decoder;
+			struct sensor_q31_data decoded_data;
 
-			rc = sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, &temp);
-			if (rc == 0) {
-				rc = sensor_channel_get(dev, SENSOR_CHAN_HUMIDITY, &hum);
-			}
+			rc = sensor_get_decoder(dev, &decoder);
 			if (rc != 0) {
-				printf("get failed: %d\n", rc);
-				break;
+				printk("%s: sensor_get_decode() failed: %d\n", dev->name, rc);
+				return rc;
 			}
 
-			printk("%16s: temp is %d.%02d °C humidity is %d.%02d %%RH\n",
-							dev->name, temp.val1, temp.val2 / 10000,
-							hum.val1, hum.val2 / 10000);
+			// struct sensor_decode_context temp_ctx =
+			// 	SENSOR_DECODE_CONTEXT_INIT(decoder, buf, SENSOR_CHAN_AMBIENT_TEMP, 0);
+			// sensor_decode(&temp_ctx, &decoded_data, 1);
+
+			uint8_t decoded_buf[128];
+			uint32_t fit = 0;
+			decoder->decode(buf, (struct sensor_chan_spec) {SENSOR_CHAN_AMBIENT_TEMP, 0},
+					&fit, 1, decoded_buf);
+
+			struct sensor_q31_data *data =
+					(struct sensor_q31_data *)decoded_buf;
+
+			printk("%16s: temp is %s%d.%d °C q temp is %d\n", dev->name,
+				PRIq_arg(data->readings[0].temperature, 6, data->shift),
+				data->readings[0].temperature);
+
+			/* original sample */
+			// rc = sensor_sample_fetch(dev);
+			// if (rc < 0) {
+			// 	printk("%s: sensor_sample_fetch() failed: %d\n", dev->name, rc);
+			// 	return rc;
+			// }
+
+			// struct sensor_value temp;
+			// struct sensor_value hum;
+
+			// rc = sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, &temp);
+			// if (rc == 0) {
+			// 	rc = sensor_channel_get(dev, SENSOR_CHAN_HUMIDITY, &hum);
+			// }
+			// if (rc != 0) {
+			// 	printf("get failed: %d\n", rc);
+			// 	break;
+			// }
+
+			// printk("%16s: temp is %d.%02d °C humidity is %d.%02d %%RH\n",
+			// 				dev->name, temp.val1, temp.val2 / 10000,
+			// 				hum.val1, hum.val2 / 10000);
 		}
 		k_msleep(1000);
 	}
