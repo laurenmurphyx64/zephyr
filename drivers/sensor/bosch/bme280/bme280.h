cb6a4a6dd4eff20af33e297a4793a0a553949c94
Lauren Murphy <lauren.murphy@intel.com>
2024-08-26 15:13:51 -0700

    drivers: sensor: bme280: use int math in decode q31 conv
    Fix decoder to use integer math when converting readings to Q31.
    Clang format files.
    
    Fixes #77295
    
    Signed-off-by: Lauren Murphy <lauren.murphy@intel.com>

--- drivers/sensor/bosch/bme280/bme280.h
+++ drivers/sensor/bosch/bme280/bme280.h
@@ -32,10 +32,9 @@ union bme280_bus {
 };
 
 typedef int (*bme280_bus_check_fn)(const union bme280_bus *bus);
-typedef int (*bme280_reg_read_fn)(const union bme280_bus *bus,
-				  uint8_t start, uint8_t *buf, int size);
-typedef int (*bme280_reg_write_fn)(const union bme280_bus *bus,
-				   uint8_t reg, uint8_t val);
+typedef int (*bme280_reg_read_fn)(const union bme280_bus *bus, uint8_t start, uint8_t *buf,
+				  int size);
+typedef int (*bme280_reg_write_fn)(const union bme280_bus *bus, uint8_t reg, uint8_t val);
 
 struct bme280_bus_io {
 	bme280_bus_check_fn check;
@@ -44,8 +43,7 @@ struct bme280_bus_io {
 };
 
 #if BME280_BUS_SPI
-#define BME280_SPI_OPERATION (SPI_WORD_SET(8) | SPI_TRANSFER_MSB |	\
-			      SPI_MODE_CPOL | SPI_MODE_CPHA)
+#define BME280_SPI_OPERATION (SPI_WORD_SET(8) | SPI_TRANSFER_MSB | SPI_MODE_CPOL | SPI_MODE_CPHA)
 extern const struct bme280_bus_io bme280_bus_io_spi;
 #endif
 
@@ -53,28 +51,53 @@ extern const struct bme280_bus_io bme280_bus_io_spi;
 extern const struct bme280_bus_io bme280_bus_io_i2c;
 #endif
 
-#define BME280_REG_PRESS_MSB            0xF7
-#define BME280_REG_COMP_START           0x88
-#define BME280_REG_HUM_COMP_PART1       0xA1
-#define BME280_REG_HUM_COMP_PART2       0xE1
-#define BME280_REG_ID                   0xD0
-#define BME280_REG_CONFIG               0xF5
-#define BME280_REG_CTRL_MEAS            0xF4
-#define BME280_REG_CTRL_HUM             0xF2
-#define BME280_REG_STATUS               0xF3
-#define BME280_REG_RESET                0xE0
+#define BME280_REG_PRESS_MSB      0xF7
+#define BME280_REG_COMP_START     0x88
+#define BME280_REG_HUM_COMP_PART1 0xA1
+#define BME280_REG_HUM_COMP_PART2 0xE1
+#define BME280_REG_ID             0xD0
+#define BME280_REG_CONFIG         0xF5
+#define BME280_REG_CTRL_MEAS      0xF4
+#define BME280_REG_CTRL_HUM       0xF2
+#define BME280_REG_STATUS         0xF3
+#define BME280_REG_RESET          0xE0
 
-#define BMP280_CHIP_ID_SAMPLE_1         0x56
-#define BMP280_CHIP_ID_SAMPLE_2         0x57
-#define BMP280_CHIP_ID_MP               0x58
-#define BME280_CHIP_ID                  0x60
-#define BME280_MODE_SLEEP               0x00
-#define BME280_MODE_FORCED              0x01
-#define BME280_MODE_NORMAL              0x03
-#define BME280_SPI_3W_DISABLE           0x00
-#define BME280_CMD_SOFT_RESET           0xB6
-#define BME280_STATUS_MEASURING         0x08
-#define BME280_STATUS_IM_UPDATE         0x01
+#define BMP280_CHIP_ID_SAMPLE_1 0x56
+#define BMP280_CHIP_ID_SAMPLE_2 0x57
+#define BMP280_CHIP_ID_MP       0x58
+#define BME280_CHIP_ID          0x60
+#define BME280_MODE_SLEEP       0x00
+#define BME280_MODE_FORCED      0x01
+#define BME280_MODE_NORMAL      0x03
+#define BME280_SPI_3W_DISABLE   0x00
+#define BME280_CMD_SOFT_RESET   0xB6
+#define BME280_STATUS_MEASURING 0x08
+#define BME280_STATUS_IM_UPDATE 0x01
+
+/* (min, max) temp device can measure is (-40, 85 C)
+ * To convert reading to Q7.24 with range -128, 128,
+ * (reading * RES) * pow(2, 31) / 128
+ * = (reading * .01) * pow(2, 31) / 128
+ * = reading * 167772.16
+ */
+#define BME280_TEMP_CONV_Q7_24   167772
+#define BME280_TEMP_SHIFT        7
+/* (300, 1100 hPa)
+ * To convert reading from UQ24.8 to Q11.20 with range
+ * -2048 to 2048,
+ * = (reading / 256 / 1000 (to hPa)) * pow(2, 31) / 2048
+ * = reading * 4.096
+ */
+#define BME280_PRESS_CONV_Q11_20 4
+#define BME280_PRESS_SHIFT       11
+/* (0, 100)
+ * To convert reading from UQ22.10 to Q7.24 with range
+ * -128, 128,
+ * (reading / 1024) * pow(2, 31) / 128
+ * = reading * 16384
+ */
+#define BME280_HUM_CONV_Q7_24    16384
+#define BME280_HUM_SHIFT         7
 
 #if defined CONFIG_BME280_MODE_NORMAL
 #define BME280_MODE BME280_MODE_NORMAL
@@ -83,82 +106,75 @@ extern const struct bme280_bus_io bme280_bus_io_i2c;
 #endif
 
 #if defined CONFIG_BME280_TEMP_OVER_1X
-#define BME280_TEMP_OVER                (1 << 5)
+#define BME280_TEMP_OVER (1 << 5)
 #elif defined CONFIG_BME280_TEMP_OVER_2X
-#define BME280_TEMP_OVER                (2 << 5)
+#define BME280_TEMP_OVER (2 << 5)
 #elif defined CONFIG_BME280_TEMP_OVER_4X
-#define BME280_TEMP_OVER                (3 << 5)
+#define BME280_TEMP_OVER (3 << 5)
 #elif defined CONFIG_BME280_TEMP_OVER_8X
-#define BME280_TEMP_OVER                (4 << 5)
+#define BME280_TEMP_OVER (4 << 5)
 #elif defined CONFIG_BME280_TEMP_OVER_16X
-#define BME280_TEMP_OVER                (5 << 5)
+#define BME280_TEMP_OVER (5 << 5)
 #endif
 
 #if defined CONFIG_BME280_PRESS_OVER_1X
-#define BME280_PRESS_OVER               (1 << 2)
+#define BME280_PRESS_OVER (1 << 2)
 #elif defined CONFIG_BME280_PRESS_OVER_2X
-#define BME280_PRESS_OVER               (2 << 2)
+#define BME280_PRESS_OVER (2 << 2)
 #elif defined CONFIG_BME280_PRESS_OVER_4X
-#define BME280_PRESS_OVER               (3 << 2)
+#define BME280_PRESS_OVER (3 << 2)
 #elif defined CONFIG_BME280_PRESS_OVER_8X
-#define BME280_PRESS_OVER               (4 << 2)
+#define BME280_PRESS_OVER (4 << 2)
 #elif defined CONFIG_BME280_PRESS_OVER_16X
-#define BME280_PRESS_OVER               (5 << 2)
+#define BME280_PRESS_OVER (5 << 2)
 #endif
 
 #if defined CONFIG_BME280_HUMIDITY_OVER_1X
-#define BME280_HUMIDITY_OVER            1
+#define BME280_HUMIDITY_OVER 1
 #elif defined CONFIG_BME280_HUMIDITY_OVER_2X
-#define BME280_HUMIDITY_OVER            2
+#define BME280_HUMIDITY_OVER 2
 #elif defined CONFIG_BME280_HUMIDITY_OVER_4X
-#define BME280_HUMIDITY_OVER            3
+#define BME280_HUMIDITY_OVER 3
 #elif defined CONFIG_BME280_HUMIDITY_OVER_8X
-#define BME280_HUMIDITY_OVER            4
+#define BME280_HUMIDITY_OVER 4
 #elif defined CONFIG_BME280_HUMIDITY_OVER_16X
-#define BME280_HUMIDITY_OVER            5
+#define BME280_HUMIDITY_OVER 5
 #endif
 
 #if defined CONFIG_BME280_STANDBY_05MS
-#define BME280_STANDBY                  0
+#define BME280_STANDBY 0
 #elif defined CONFIG_BME280_STANDBY_62MS
-#define BME280_STANDBY                  (1 << 5)
+#define BME280_STANDBY (1 << 5)
 #elif defined CONFIG_BME280_STANDBY_125MS
-#define BME280_STANDBY                  (2 << 5)
+#define BME280_STANDBY (2 << 5)
 #elif defined CONFIG_BME280_STANDBY_250MS
-#define BME280_STANDBY                  (3 << 5)
+#define BME280_STANDBY (3 << 5)
 #elif defined CONFIG_BME280_STANDBY_500MS
-#define BME280_STANDBY                  (4 << 5)
+#define BME280_STANDBY (4 << 5)
 #elif defined CONFIG_BME280_STANDBY_1000MS
-#define BME280_STANDBY                  (5 << 5)
+#define BME280_STANDBY (5 << 5)
 #elif defined CONFIG_BME280_STANDBY_2000MS
-#define BME280_STANDBY                  (6 << 5)
+#define BME280_STANDBY (6 << 5)
 #elif defined CONFIG_BME280_STANDBY_4000MS
-#define BME280_STANDBY                  (7 << 5)
+#define BME280_STANDBY (7 << 5)
 #endif
 
 #if defined CONFIG_BME280_FILTER_OFF
-#define BME280_FILTER                   0
+#define BME280_FILTER 0
 #elif defined CONFIG_BME280_FILTER_2
-#define BME280_FILTER                   (1 << 2)
+#define BME280_FILTER (1 << 2)
 #elif defined CONFIG_BME280_FILTER_4
-#define BME280_FILTER                   (2 << 2)
+#define BME280_FILTER (2 << 2)
 #elif defined CONFIG_BME280_FILTER_8
-#define BME280_FILTER                   (3 << 2)
+#define BME280_FILTER (3 << 2)
 #elif defined CONFIG_BME280_FILTER_16
-#define BME280_FILTER                   (4 << 2)
+#define BME280_FILTER (4 << 2)
 #endif
 
-#define BME280_CTRL_MEAS_VAL            (BME280_PRESS_OVER | \
-					 BME280_TEMP_OVER |  \
-					 BME280_MODE)
-#define BME280_CONFIG_VAL               (BME280_STANDBY | \
-					 BME280_FILTER |  \
-					 BME280_SPI_3W_DISABLE)
+#define BME280_CTRL_MEAS_VAL (BME280_PRESS_OVER | BME280_TEMP_OVER | BME280_MODE)
+#define BME280_CONFIG_VAL    (BME280_STANDBY | BME280_FILTER | BME280_SPI_3W_DISABLE)
 
-
-#define BME280_CTRL_MEAS_OFF_VAL	(BME280_PRESS_OVER | \
-					 BME280_TEMP_OVER |  \
-					 BME280_MODE_SLEEP)
+#define BME280_CTRL_MEAS_OFF_VAL (BME280_PRESS_OVER | BME280_TEMP_OVER | BME280_MODE_SLEEP)
 
 struct bme280_reading {
 	/* Compensated values. */
@@ -221,11 +237,9 @@ int bme280_get_decoder(const struct device *dev, const struct sensor_decoder_api
 
 void bme280_submit(const struct device *dev, struct rtio_iodev_sqe *iodev_sqe);
 
-int bme280_sample_fetch(const struct device *dev,
-			       enum sensor_channel chan);
+int bme280_sample_fetch(const struct device *dev, enum sensor_channel chan);
 
-int bme280_sample_fetch_helper(const struct device *dev,
-			       enum sensor_channel chan,
+int bme280_sample_fetch_helper(const struct device *dev, enum sensor_channel chan,
 			       struct bme280_reading *reading);
 
 #endif /* ZEPHYR_DRIVERS_SENSOR_BME280_BME280_H_ */
