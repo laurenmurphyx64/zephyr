be672b6693c7d5a38718505434d1fad7562c2f82
Lauren Murphy <lauren.murphy@intel.com>
2025-05-23 17:12:25 -0700

    llext: compile llext with arc mwdt on nsim vpx5
    Allow users to compile LLEXTs with the MetaWare Design Toolchain.
    Provides utility to dramatically shrink bloated extensions by removing
    unused section names left behind by MWDT strip. Test extensions will
    not fit in NSIM VPX5 memory otherwise; each is reduced from ~16k to ~1k.
    
    Signed-off-by: Lauren Murphy <lauren.murphy@intel.com>

---
+++ scripts/build/llext_elf_editor.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2025 Intel Corporation
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""Functions to rewrite ELF files.
+
+Given a bytearray read from an ELF file containing one or more entries
+from a table (ELF header entry, section header entry, or symbol), edit
+fields inline. User will write this bytearray back to the file.
+"""
+
+from elftools.elf.elffile import ELFFile
+
+def get_field_size(struct, name):
+    for field in struct.subcons:
+        if field.name == name:
+            return field.sizeof()
+
+    raise ValueError(f"Unknown field name: {name}")
+
+def get_field_offset(struct, name):
+    offset = 0
+
+    for field in struct.subcons:
+        if field.name == name:
+            break
+        offset += field.sizeof()
+
+    if offset < struct.sizeof():
+        return offset
+
+    raise ValueError(f"Unknown field name: {name}")
+
+def write_field_in_struct_bytearr(elf, bytearr, name, num, ent_idx = 0):
+    if name[0] == 'e':
+        header = elf.structs.Elf_Ehdr
+    elif name[0:2] == 'sh':
+        header = elf.structs.Elf_Shdr
+    elif name[0:2] == 'st':
+        header = elf.structs.Elf_Sym
+    else:
+        raise ValueError(f"Unable to identify struct for {name}")
+
+    size = get_field_size(header, name)
+
+    # ent_idx is 0 for ELF header
+    offset = (ent_idx * header.sizeof()) + get_field_offset(header, name)
+
+    field = None
+    if elf.little_endian:
+        field = num.to_bytes(size, 'little')
+    else:
+        field = num.to_bytes(size, 'big')
+
+    bytearr[offset:offset+size] = field
