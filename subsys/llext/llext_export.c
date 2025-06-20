8b50517c7114a7a685b7c7136abc882f0afe182a
Lauren Murphy <lauren.murphy@intel.com>
2025-06-20 15:59:15 -0700

    raaaaaaa

--- subsys/llext/llext_export.c
+++ subsys/llext/llext_export.c
@@ -6,7 +6,6 @@
 
 #include <string.h>
 #include <zephyr/llext/symbol.h>
-// #include <zephyr/offsets.h>
 
 EXPORT_SYMBOL(strcpy);
 EXPORT_SYMBOL(strncpy);
@@ -17,8 +16,20 @@ EXPORT_SYMBOL(memcmp);
 EXPORT_SYMBOL(memcpy);
 EXPORT_SYMBOL(memset);
 
-// #ifdef __CCAC__
-// EXPORT_SYMBOL(__ac_mc_va)
-// #endif
+#ifdef __CCAC__
+extern void __ac_mc_va(void);
+extern void __ac_pop_13_to_14v(void);
+extern void __ac_push_13_to_16(void);
+extern void __ac_pop_13_to_16v(void);
+extern void __ac_push_13_to_17(void);
+extern void __ac_pop_13_to_17v(void);
+
+EXPORT_SYMBOL(__ac_mc_va);
+EXPORT_SYMBOL(__ac_pop_13_to_14v);
+EXPORT_SYMBOL(__ac_push_13_to_16);
+EXPORT_SYMBOL(__ac_pop_13_to_16v);
+EXPORT_SYMBOL(__ac_push_13_to_17);
+EXPORT_SYMBOL(__ac_pop_13_to_17v);
+#endif
 
 #include <zephyr/syscall_exports_llext.c>
