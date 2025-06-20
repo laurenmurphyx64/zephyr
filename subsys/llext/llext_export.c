6f01bad3a04d0aaf09ce2ede02a0420c0bf46778
Lauren Murphy <lauren.murphy@intel.com>
2025-06-19 18:46:12 -0700

    the mystery of the __ac_mc_va fsymbol

--- subsys/llext/llext_export.c
+++ subsys/llext/llext_export.c
@@ -6,6 +6,7 @@
 
 #include <string.h>
 #include <zephyr/llext/symbol.h>
+// #include <zephyr/offsets.h>
 
 EXPORT_SYMBOL(strcpy);
 EXPORT_SYMBOL(strncpy);
@@ -15,5 +16,9 @@ EXPORT_SYMBOL(strncmp);
 EXPORT_SYMBOL(memcmp);
 EXPORT_SYMBOL(memcpy);
 EXPORT_SYMBOL(memset);
+
+// #ifdef __CCAC__
+// EXPORT_SYMBOL(__ac_mc_va)
+// #endif
 
 #include <zephyr/syscall_exports_llext.c>
